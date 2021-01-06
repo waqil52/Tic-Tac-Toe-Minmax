abad7d60f20bf3ebb8d2201e1f825b7d36716b09
Waqil Shopnil <waqilshopnil@gmail.com>
2021-01-06 23:03:49 +0600

    added missing winning combos to finish the game statically

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ var origBoard;
 const huPlayer = "O";
 const aiPlayer = "X";
 const winCombos = [ //the combination of cell numbers for a player or ai to win
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
 	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[6, 4, 2]
+    [6, 4, 2],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+    [7, 8, 9],
+    [4, 5, 6],
+    [1, 2, 3],
+    [2, 5, 8]
 ]
 const cells = document.querySelectorAll('.cell');
 startGame();
@@ -32,5 +33,31 @@ function startGame(){
 function turn(squareId, playerType){
     origBoard[squareId] = playerType;
     document.getElementById(squareId).innerText = playerType; //player adding his turn in each square
-    
+    let gameWon = checkWin(origBoard, playerType);
+    if(gameWon){
+        gameOver(gameWon);
+    }
+}
+
+function checkWin(board, playerType){
+    let plays = board.reduce((a, e, i) => 
+        (e === playerType) ? a.concat(i) : a, []); //storing the entries in the cells
+    console.log(plays);
+    let gameWon = null;
+    for(let [index, win] of winCombos.entries()){ //entering every win combination and getting the index and value
+        if(win.every(element => plays.indexOf(element) > -1)){ //checking every value in a combination has been satisfied by player
+            gameWon = {index: index, playerType: playerType}; //returning the winning combination and the playertype
+            break;
+        }
+    }
+    return gameWon;
+}
+
+function gameOver(gameWon){
+    for(let index of winCombos[gameWon.index]){
+        document.getElementById(index).style.backgroundColor =gameWon.playerType == huPlayer? "blue" : "red"; //coloring the winning combos of cells from both players red or blue
+    }
+    for(var i =0; i < cells.length;i++){
+        cells[i].removeEventListener("click",turnClick, false);
+    }
 }
